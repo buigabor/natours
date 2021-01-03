42d0e11bdcabda3ab330c98753c6544f3332cb99
Bui Gábor <bui.gabor97@gmail.com>
2021-01-03 19:38:56 +0100

    Handle validation, duplication and cast errors

--- back-end/controllers/errorController.js
+++ back-end/controllers/errorController.js
@@ -1,3 +1,26 @@
+const AppError = require('../utils/appError');
+
+const handleValidationErrorDB = (err) => {
+  const values = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${values.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  //Find string between quotes -> extracting the value from errmsg property
+  // const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const duplicateError = err.keyValue;
+  const key = Object.keys(duplicateError);
+  const value = Object.values(duplicateError);
+  const message = `Duplicate ${key}: ${value}, please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -9,7 +32,7 @@ const sendErrorForDev = (err, res) => {
 
 const sendErrorForProd = (err, res) => {
   // Operational error
-  if (err.isOperational) {
+  if (err.isOperationalError) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
@@ -29,7 +52,16 @@ const globalErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorForDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorForProd(err, res);
+    let error = { ...err };
+    console.log(error);
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    } else if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    } else if (err.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+    sendErrorForProd(error, res);
   }
 };
 
