fffa26c18dbae873286a56814193a7b40cea9f6a
Bui Gábor <bui.gabor97@gmail.com>
2021-01-04 17:05:02 +0100

    Add uncaught exception and promise rejection handling

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (error) => {
+  console.log(error);
+  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN...');
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -20,6 +27,14 @@ mongoose
     console.log('DB connection succesful!');
   });
 
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Server up on port 3000');
+});
+
+process.on('unhandledRejection', (error) => {
+  console.log(error);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
