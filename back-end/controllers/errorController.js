c2cce9964263475bd43940747d3a8098106603f9
Bui Gábor <bui.gabor97@gmail.com>
2021-01-04 17:05:46 +0100

    Handle JWT errors

--- back-end/controllers/errorController.js
+++ back-end/controllers/errorController.js
@@ -1,5 +1,11 @@
 const AppError = require('../utils/appError');
 
+const handleJWTExpirationError = () =>
+  new AppError('Your token has expired. Please login again', 401);
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please login again', 401);
+
 const handleValidationErrorDB = (err) => {
   const values = Object.values(err.errors).map((el) => el.message);
   const message = `Invalid input data. ${values.join('. ')}`;
@@ -60,6 +66,10 @@ const globalErrorHandler = (err, req, res, next) => {
       error = handleDuplicateFieldsDB(error);
     } else if (err.name === 'ValidationError') {
       error = handleValidationErrorDB(error);
+    } else if (err.name === 'JsonWebTokenError') {
+      error = handleJWTError();
+    } else if (err.name === 'TokenExpiredError') {
+      error = handleJWTExpirationError();
     }
     sendErrorForProd(error, res);
   }
