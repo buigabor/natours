bfd8bc80910c8c9d5c87f6373bd14936691cbc72
Bui Gábor <bui.gabor97@gmail.com>
2021-01-05 08:31:50 +0100

    Add protect, restrict to, login, signup, forgotpw and resetpw

--- back-end/routes/tourRoutes.js
+++ back-end/routes/tourRoutes.js
@@ -10,13 +10,19 @@ const {
   getMonthlyPlan,
 } = require('../controllers/tourController');
 
+const { protectRoute, restrictTo } = require('../controllers/authController');
+
 const router = express.Router();
 
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').patch(updateTour).delete(deleteTour).get(getTour);
+router.route('/').get(protectRoute, getAllTours).post(createTour);
+router
+  .route('/:id')
+  .patch(updateTour)
+  .delete(protectRoute, restrictTo('admin', 'lead-guide'), deleteTour)
+  .get(getTour);
 
 module.exports = router;
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -6,8 +6,20 @@ const {
   deleteUser,
   updateUser,
 } = require('../controllers/userController');
+const {
+  signUp,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', signUp);
+router.post('/login', login);
+
+router.post('/forgotPassword', forgotPassword);
+router.patch('/resetPassword/:token', resetPassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
