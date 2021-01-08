f47f8ab9e2049b545124f9c43854ff9cde4d0ca6
Bui Gábor <bui.gabor97@gmail.com>
2021-01-08 09:31:18 +0100

    Solidify security

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const path = require('path');
 const morgan = require('morgan');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 const AppError = require('./utils/appError');
@@ -8,22 +14,68 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
-// MIDDLEWARES
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1. GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// Set maximum of 100 requests in 1 hour
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again an hour later',
+});
+
+// use it for every route starting with /api
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitazation against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitazation against XSS attacks
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // ROUTES
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl}`, 404));
