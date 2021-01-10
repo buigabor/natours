37c9301846256e84563c8408e455c8142aa566e2
Bui Gábor <bui.gabor97@gmail.com>
2021-01-10 22:02:45 +0100

    Add cookie parser

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const AppError = require('./utils/appError');
@@ -40,6 +41,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitazation against NOSQL query injection
 app.use(mongoSanitize());
@@ -67,6 +69,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
